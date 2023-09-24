175f0de2a8bd861a646cbd18403d27905e4a90b5
RafalPelc <119452508+RafalPelc@users.noreply.github.com>
2023-09-24 11:42:05 +0000

    Deploying to gh-pages from @ RafalPelc/goit-react-hw-05-movies@a624f5a4100845cc0e9136e9069ba2e162021d2c 🚀

---
+++ static/js/805.112c1406.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[805],{805:function(e,n,t){t.r(n);var o=t(87),r=t(184);n.default=function(){return(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)("h2",{children:"Oops.. nothing found..."}),(0,r.jsx)(o.rU,{to:"/",children:"Back to home"})]})}}}]);
+//# sourceMappingURL=805.112c1406.chunk.js.map
